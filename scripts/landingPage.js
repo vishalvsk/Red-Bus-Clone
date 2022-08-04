8f1a424190cc8c2165ae202eb1332f6a9ea34cc8
vishal Karale <107266771+vishalvsk@users.noreply.github.com>
2022-08-04 18:49:48 +0530

    landingPageJs

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -1,4 +1,26 @@
-  let from,to,onwardDate,returnDate;
+ function animate() {
+   var reveals = document.querySelectorAll(".animate");
+
+   for (var i = 0; i < reveals.length; i++) {
+     var windowHeight = window.innerHeight;
+     var elementTop = reveals[i].getBoundingClientRect().top;
+     var elementVisible = 50;
+
+     if (elementTop < windowHeight - elementVisible) {
+       reveals[i].classList.add("active");
+     } else {
+       reveals[i].classList.remove("active");
+     }
+   }
+ } 
+  
+  
+  
+
+
+
+
+let from, to, onwardDate, returnDate;
     function searchBtn() {
         from = document.querySelector("#input-label-from").value;
         to = document.querySelector("#input-label-to").value;
