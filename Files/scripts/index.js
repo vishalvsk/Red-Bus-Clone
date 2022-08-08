70a0751611ab3f9240c4a71dabe7bd3ab90f62d1
vishal Karale <vishalkaralevsk@gmail.com>
2022-08-08 15:30:39 +0530

    Changes done in Vishal Branch

---
+++ Files/scripts/index.js
@@ -0,0 +1,54 @@
+ function animate() {
+   var reveals = document.querySelectorAll(".animate");
+
+   for (var i = 0; i < reveals.length; i++) {
+     var windowHeight = window.innerHeight;
+     var elementTop = reveals[i].getBoundingClientRect().top;
+     var elementVisible = 50;
+
+     if (elementTop < windowHeight - elementVisible) {
+       reveals[i].classList.add("active");
+     } else {
+       reveals[i].classList.remove("active");
+     }
+   }
+ } 
+  
+  
+  
+
+
+
+
+let from, to, onwardDate, returnDate;
+    function searchBtn() {
+        from = document.querySelector("#input-label-from").value;
+        to = document.querySelector("#input-label-to").value;
+        onwardDate = document.querySelector("#input-label-onward-date").value;
+        returnDate = document.querySelector("#input-label-return-date").value;
+
+        localStorage.setItem("from",from);
+        localStorage.setItem("to",to);
+        localStorage.setItem("onwardDate",onwardDate);
+        localStorage.setItem("returnDate",returnDate);
+        
+    }
+
+
+    function myFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function (event) {
+      if (!event.target.matches("#dropBtn")) {
+        var dropdowns = document.getElementsByClassName("dropdown-menu");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains("show")) {
+            openDropdown.classList.remove("show");
+          }
+        }
+      }
+    };
